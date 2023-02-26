93fc4e59dad183da8909c00abde7689ff9d36ac2
yolish <yolisha@gmail.com>
2023-02-26 09:39:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -108,6 +108,30 @@ if __name__ == "__main__":
         model = BrRwlFormer(config, args.rpr_backbone_path).to(device)
     elif arch == "deltanet":
         model = DeltaNet(config, args.rpr_backbone_path).to(device)
+        # support freeze
+        estimate_position_with_prior = config.get("position_with_prior")
+        estimate_rotation_with_prior = config.get("rotation_with_prior")
+        freeze = False
+        if estimate_rotation_with_prior:
+            freeze = True
+            # exclude rotation-related
+            freeze_exclude_phrase = ["_q."] # freeze backbone and all position-related modules
+        elif estimate_position_with_prior:
+            freeze = True
+            # exclude position-related
+            freeze_exclude_phrase = ["_x."] # freeze backbone and all rotation-related modules
+        if freeze:
+            for name, parameter in model.named_parameters():
+                freeze_param = True
+                for phrase in freeze_exclude_phrase:
+                    if phrase in name:
+                        freeze_param = False
+                        break
+                if freeze_param:
+                    parameter.requires_grad_(False)
+                else:
+                    print(name)
+
     elif arch == "baseline":
         model = BaselineRPR(args.rpr_backbone_path).to(device)
     elif arch == "deltanetequiv":
